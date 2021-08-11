5a7bece725860ec4ea239a3f6cf2109fe2c9d121
SphericalKat <amolele@gmail.com>
2021-08-12 02:47:23 +0530

    feat(ui): implement header ui
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,11 +1,20 @@
 module.exports = {
-  purge: [],
+  purge: [
+    "../lib/**/*.ex",
+    "../lib/**/*.leex",
+    "../lib/**/*.eex",
+    "./js/**/*.js",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+			colors: {
+        amber: '#ff9800',
+      },
+		},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
