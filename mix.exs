babcfd62a38fe1e23a621bb30aa0c0e1811e6091
SphericalKat <amolele@gmail.com>
2021-08-11 12:25:21 +0530

    chore(mix): add phx_gen_auth to dependencies
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule Ketbin.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:phx_gen_auth, "~> 0.7", only: [:dev], runtime: false},
     ]
   end
 
