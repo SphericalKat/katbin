425fb2b57151550ff3b5fe1b5db0840340687343
SphericalKat <amolele@gmail.com>
2021-08-17 02:05:58 +0530

    feat(pastes): use rustler for highlight NIF
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- lib/ketbin/utils/syntax.ex
+++ lib/ketbin/utils/syntax.ex
@@ -2,5 +2,9 @@ defmodule Ketbin.Utils.Syntax do
   use Rustler, otp_app: :ketbin, crate: "ketbin_utils_syntax"
 
   # When your NIF is loaded, it will override this function.
-  def add(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
+  def add(_a, _b), do: error()
+
+  def highlight_text(_text, _lang), do: error()
+
+  defp error(), do: :erlang.nif_error(:nif_not_loaded)
 end
--- native/ketbin_utils_syntax/src/lib.rs
+++ native/ketbin_utils_syntax/src/lib.rs
@@ -1,6 +1,26 @@
+use syntect::{html::{ClassStyle, ClassedHTMLGenerator}, parsing::SyntaxSet, util::LinesWithEndings};
+
 #[rustler::nif]
 fn add(a: i64, b: i64) -> i64 {
     a + b
 }
 
-rustler::init!("Elixir.Ketbin.Utils.Syntax", [add]);
+#[rustler::nif]
+pub fn highlight_text(text: String, lang: String) -> String {
+	let syntax_set = SyntaxSet::load_defaults_newlines();
+
+	let syntax = syntax_set
+			.find_syntax_by_extension(&lang)
+			.unwrap_or(syntax_set.find_syntax_plain_text());
+
+	let mut rs_html_generator =
+			ClassedHTMLGenerator::new_with_class_style(syntax, &syntax_set, ClassStyle::Spaced);
+
+	for line in LinesWithEndings::from(&text) {
+			rs_html_generator.parse_html_for_line_which_includes_newline(&line)
+	}
+
+	rs_html_generator.finalize()
+}
+
+rustler::init!("Elixir.Ketbin.Utils.Syntax", [add, highlight_text]);
