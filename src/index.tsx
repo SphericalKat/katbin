1d6153d6de36d320ac7f33e3bcbce7d40c1c9db3
SphericalKat <amolele@gmail.com>
2026-09-01 23:55:15 +0530

    fix: restore paste edit icon

--- src/index.tsx
+++ src/index.tsx
@@ -1053,12 +1053,23 @@ const PastePage: FC<{
         ) : error ? (
           <p class="alert alert-danger">{error}</p>
         ) : null}
-        {showEdit ? (
-          <a class="absolute right-0 top-0 p-4" href={`/edit/${id}`}>
-            Edit
-          </a>
-        ) : null}
-        <PasteContent content={content} extension={extension} />
+        <div class="relative flex h-full w-full flex-col">
+          {showEdit ? (
+            <div class="absolute right-0 top-0 flex p-4">
+              <a href={`/edit/${id}`} aria-label="Edit paste" title="Edit paste">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  class="h-6 w-6 cursor-pointer fill-current text-white"
+                  aria-hidden="true"
+                >
+                  <path d="M3 17.46v3.04c0 .28.22.5.5.5h3.04c.13 0 .26-.05.35-.15L17.81 9.94l-3.75-3.75L3.15 17.1c-.1.1-.15.22-.15.36zM20.71 7.04a.996.996 0 0 0 0-1.41l-2.34-2.34a.996.996 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" />
+                </svg>
+              </a>
+            </div>
+          ) : null}
+          <PasteContent content={content} extension={extension} />
+        </div>
       </main>
       <Footer />
     </body>
